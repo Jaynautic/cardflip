611ddc256fdee1caceef613fec927f1a200a6e17
Jaynautic <jaynauticstudios@gmail.com>
2024-08-21 15:24:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,62 @@ var firstCard;
 
 var numOfCards = 0;
 
+var cardHand = [];
+
 var cardValues = [];
 
 const cardList = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'];
 
 const cardSize = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 
-let dealerHand = Math.floor(Math.random() * 6) + 15;
+var dealerHand = 0;
 
-document.getElementById("result").innerHTML = `The dealer's hand is ${dealerHand}.`;
+var dealerIndex = 0;
+
+var dealerValues = [];
+
+
+
+for (let i = 0; i < 2; i++) {
+  let ranNum = Math.floor(Math.random() * cardList.length);
+  const cardId = cardList[ranNum];
+  const cardValue = cardSize[ranNum];
+  dealerValues.push(cardId);
+
+  let newCard = `<div class="dealer-card-inner" id="dealerCardinner${cardId}-${dealerIndex}" onclick="resultDealerCard('${cardId}','${dealerIndex}')">
+                  <div class="dealer-card-front"></div>
+                  <div class="dealer-card-back" id="cardface${cardId}"></div>
+                </div>`;
+
+  let dealerContainer = document.getElementById("dealerContainer");
+
+  var newDealerCard = document.createElement('div');
+  newDealerCard.id = `dealerCard${dealerIndex}`;
+  newDealerCard.setAttribute('class', 'dealer-card');
+
+  dealerContainer.appendChild(newDealerCard);
+
+  document.getElementById(`dealerCard${dealerIndex}`).innerHTML = newCard;
+
+  dealerHand = dealerHand + cardValue;
+  dealerIndex++
+
+  if (dealerIndex == 1) {
+    document.getElementById(`dealerCardinner${cardId}-${dealerIndex - 1}`).style.transform = "rotateY(180deg)";
+    document.getElementById("heading").innerHTML = `The dealer's hand starts with a ${cardValue}.`;
+  }
+}
+
+// document.getElementById("heading").innerHTML = `The dealer's hand starts at ${dealerHand}.`;
+
+
 
 for (let i = 0; i < 2; i++) {
   let ranNum = Math.floor(Math.random() * cardList.length);
   const cardId = cardList[ranNum];
   cardValues.push(cardSize[ranNum]);
-  let newCard = `<div class="card-inner" id="cardinner${cardId}-${i}" onclick="resultCard('${cardId}','${i}')">
+  cardHand.push(cardId);
+  let newCard = `<div class="card-inner" id="cardinner${cardId}-${i+1}" onclick="resultCard('${cardId}','${i+1}')">
                   <div class="card-front"></div>
                   <div class="card-back" id="cardface${cardId}"></div>
                 </div>`;
@@ -24,26 +65,30 @@ for (let i = 0; i < 2; i++) {
   numOfCards++;
 }
 
+
+
 function resultCard(data, cardNum) {
-  if(firstCard == undefined) {
-    // document.getElementById("result").innerHTML = `You chose ${data}`;
-    // document.getElementById("heading").innerHTML = `Pick another card.`;
-    document.getElementById(`cardinner${data}-${cardNum}`).style.transform = "rotateY(180deg)";
+  
+  console.log(`cardinner${data}-${cardNum}`);
 
-    firstCard = data;
+  document.getElementById(`cardinner${data}-${cardNum}`).style.transform = "rotateY(180deg)";
 
-  } else {
-    // document.getElementById("heading").innerHTML = `Your cards are ${firstCard} and ${data}.`;
-    // document.getElementById("result").innerHTML = `Your pair ID is ${firstCard + data}.`;
-    document.getElementById(`cardinner${data}-${cardNum}`).style.transform = "rotateY(180deg)";
-  }
 }
+
+function resultDealerCard(data, cardNum) {
+
+  document.getElementById(`dealerCardinner${data}-${cardNum}`).style.transform = "rotateY(180deg)";
+
+}
+
+
 
 function hitFunction() {
 
   let ranNum = Math.floor(Math.random() * cardList.length);
   const cardId = cardList[ranNum];
   cardValues.push(cardSize[ranNum]);
+  cardHand.push(cardId);
   numOfCards++;
 
   let newCard = `<div class="card-inner" id="cardinner${cardId}-${numOfCards}" onclick="resultCard('${cardId}','${numOfCards}')">
@@ -61,10 +106,44 @@ function hitFunction() {
 
   document.getElementById(`card${numOfCards}`).innerHTML = newCard;
 
+  
+
   console.log(cardValues);
+  console.log(cardHand);
+  console.log(numOfCards)
 }
 
+
+
 function standFunction() {
+
+  while (dealerHand < 16) {
+    let ranNum = Math.floor(Math.random() * cardList.length);
+    const cardId = cardList[ranNum];
+    const cardValue = cardSize[ranNum];
+    dealerValues.push(cardId);
+  
+    let newCard = `<div class="dealer-card-inner" id="dealerCardinner${cardId}-${dealerIndex}">
+                    <div class="dealer-card-back" id="cardface${cardId}"></div>
+                  </div>`;
+  
+    let dealerContainer = document.getElementById("dealerContainer");
+  
+    var newDealerCard = document.createElement('div');
+    newDealerCard.id = `dealerCard${dealerIndex}`;
+    newDealerCard.setAttribute('class', 'dealer-card');
+  
+    dealerContainer.appendChild(newDealerCard);
+  
+    document.getElementById(`dealerCard${dealerIndex}`).innerHTML = newCard;
+  
+    dealerHand = dealerHand + cardValue;
+    dealerIndex++
+
+    document.getElementById(`dealerCardinner${cardId}-${dealerIndex - 1}`).style.transform = "rotateY(180deg)";
+
+  }
+
   let cardSum = cardValues.reduce((a, b) => a + b, 0);
 
   for (let i = 0; i < cardValues.length; i++) {
@@ -74,9 +153,22 @@ function standFunction() {
   }
   cardSum = cardValues.reduce((a, b) => a + b, 0);
 
-  if (cardSum <= 21 && cardSum > dealerHand) {
+  if ((cardSum <= 21 && cardSum > dealerHand) || (cardSum <= 21 && dealerHand > 21)) {
     document.getElementById("result").innerHTML = `Your hand is ${cardSum}. You won!`;
   } else {
     document.getElementById("result").innerHTML = `You hand is ${cardSum}. You lost!`;
   }
+
+
+  for (let i = 0; i < dealerValues.length; i++) {
+    document.getElementById(`dealerCardinner${dealerValues[i]}-${i}`).style.transform = "rotateY(180deg)";
+  }
+
+  for (let i = 1; i < (cardHand.length+1); i++) {
+    let dataCard = cardHand[i-1];
+    let numberOfCards = i;
+    resultCard(dataCard, numberOfCards);
+  }
+
+  document.getElementById("heading").innerHTML = `The dealer's hand is ${dealerHand}.`;
 }
